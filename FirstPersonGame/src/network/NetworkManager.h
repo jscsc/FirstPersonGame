8544bbfa6705128eb46c7b0a408cbc9926174ca1
matt stone <mattstone22133@gmail.com>
2018-08-28 23:32:41 -0400

    started WIP network manager

--- FirstPersonGame/src/network/NetworkManager.h
+++ FirstPersonGame/src/network/NetworkManager.h
@@ -1,9 +1,99 @@
 #pragma once
+#include <string>
+#include <SFML/Network.hpp>
+
 class NetworkManager final
 {
+	bool _IsHosting = false;
+	bool _activeConnection = true;
+	unsigned short _port;
+
+	//host fields
+	sf::TcpListener hostListener;
+	bool _stopListenThread;
 
 public:
 	NetworkManager();
 	~NetworkManager();
+
+	void thread_listenForIncomingConnections()
+	{
+		//allow timeouts using the selector class
+		hostListener.setBlocking(false);
+		sf::SocketSelector selector;
+		selector.add(hostListener);
+
+		//keep listening for new connections in this thread
+		while (true)
+		{
+			//does not block when setBlocking(false)
+			hostListener.listen(_port);
+
+			//wait on accept (ie block this thread)
+			if (selector.wait(sf::seconds(1)))
+			{
+				if (selector.isReady(hostListener))
+				{
+					//connect a client
+				}
+			}
+
+			//check if there is a pending shutdown
+			if (_stopListenThread)
+			{
+				hostListener.close();
+				_stopListenThread = false;
+				threadCloseCheck();
+				break;
+			}
+		}
+	}
+
+	void threadCloseCheck()
+	{
+
+	}
+
+	void disconnect()
+	{
+		//TODO - make a test for this
+	}
+
+	bool isConnectionActive()
+	{
+		return _activeConnection;
+	}
+
+	void hostOnPort(unsigned short port)
+	{
+		// ignore if already hosting on this port
+		if (isConnectionActive() && _IsHosting && _port == port)
+		{
+			return;
+		}
+
+		//disconnect if hosting/joined on another port
+		if (isConnectionActive() && (_port != port))
+		{
+			//disconnect before connecting
+			disconnect();
+		}
+		
+		_IsHosting = true;
+		_port = port;
+		_activeConnection = true;
+
+		//start listening thread
+		
+		
+	}
+
+	void connectToHost(std::string ip, unsigned short port)
+	{
+
+	}
+
+private:
+
 };
 
---
+++ FirstPersonGame/src/tests/NetworkManagerConnectTest.h
@@ -0,0 +1,31 @@
+#pragma once
+#pragma once
+#include "TestBase.h"
+#include <SFML/Network.hpp>
+#include <thread>
+#include "../network/NetworkManager.h"
+
+
+class NetworkManagerConnectTest: public TestBase
+{
+
+public:
+
+	NetworkManagerConnectTest() {}
+	~NetworkManagerConnectTest() {}
+
+	virtual bool run()
+	{
+		AutoLogger logger(_passedTest, "network connection");
+
+		NetworkManager host;
+		host.hostOnPort(25565);
+
+		NetworkManager client;
+		client.connectToHost("125.125.125.125", 25565);
+
+		return _passedTest;
+	}
+
+};
+
